91476581be86bbe00c8fc12c502a80111c4b69f8
Steven Newton <newton36@gmail.com>
2025-03-29 18:22:59 -0400

    Update data.py

--- data.py
+++ data.py
@@ -1,57 +1,57 @@
 data = [
   
-  {"name": "William Bradford",
-   "lat": 53.521446953950715,
-   "lon": -2.363069184807771,
-   "year": 1625,
-   "color": "red"},
-     
-  {"name": "Wilhelm Constanz",
-   "lat": 50.742462619040104, 
-   "lon": 9.197418362001574,
-   "year": 1900,
-   "color": "blue"},
+        {"name": "William Bradford",
+         "lat": 53.521446953950715,
+         "lon": -2.363069184807771,
+         "year": 1625,
+         "color": "red"},
+           
+        {"name": "Wilhelm Constanz",
+         "lat": 50.742462619040104,
+         "lon": 9.197418362001574,
+         "year": 1900,
+         "color": "blue"},
+            
+        {"name": "Samuel Parsons",
+         "lat": 40.89766494234153,
+         "lon": -72.73183136944137,
+         "year": 1675,
+         "color": "red"},
+        
+        {"name": "William Whiting",
+         "lat": 41.9526325983207,
+         "lon": -70.70019408877086,
+         "year": 1625,
+         "color": "red"},
+           
+        {"name": "Thomas Green",
+         "lat": 42.37209520543502,
+         "lon": -71.19339074182047,
+         "year": 1900,
+         "color": "green"},
+            
+        {"name": "John Davis Parsons",
+         "lat": 42.445977163003334,
+         "lon": -73.79025253159793,
+         "year": 1675,
+         "color": "green"},
       
-  {"name": "Samuel Parsons",
-   "lat": 40.89766494234153, 
-   "lon": -72.73183136944137,
-   "year": 1675,
-   "color": "red"}
+        {"name": "Charles Newton",
+         "lat": 42.5188538275018,
+         "lon": -73.78316775188941,
+         "year": 1625,
+         "color": "green"},
+           
+        {"name": "John P. Newton Sr.",
+         "lat": 42.514278766681834,
+         "lon": -73.78251190293047,
+         "year": 1900,
+         "color": "blue"},
+            
+        {"name": "George Muse",
+         "lat": 38.279434519539805,
+         "lon": -78.5034756345365,
+         "year": 1675,
+         "color": "red"}
   
-  {"name": "William Whiting",
-   "lat": 41.9526325983207, 
-   "lon": -70.70019408877086,
-   "year": 1625,
-   "color": "red"},
-     
-  {"name": "Thomas Green",
-   "lat": 42.37209520543502,
-   "lon": -71.19339074182047,
-   "year": 1900,
-   "color": "green"},
-      
-  {"name": "John Davis Parsons",
-   "lat": 42.445977163003334,
-   "lon": -73.79025253159793,
-   "year": 1675,
-   "color": "green"}
-
-  {"name": "Charles Newton",
-   "lat": 42.5188538275018,
-   "lon": -73.78316775188941,
-   "year": 1625,
-   "color": "green"},
-     
-  {"name": "John P. Newton Sr.",
-   "lat": 42.514278766681834,
-   "lon": -73.78251190293047,
-   "year": 1900,
-   "color": "blue"},
-      
-  {"name": "George Muse",
-   "lat": 38.279434519539805,
-   "lon": -78.5034756345365,
-   "year": 1675,
-   "color": "red"}
-
-]
+        ]
