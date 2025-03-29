9cc8dc2bd3573bc17e819decf1f4a97b5f603a64
Steven Newton <newton36@gmail.com>
2025-03-29 18:10:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from data import data
 import folium
 import os
 
@@ -8,28 +9,7 @@ app = Flask(__name__)
 def index():
     # Initialize map with a global center (adjust based on your data's focus)
     m = folium.Map(location=[20, 0], zoom_start=2)
-
-    # Genealogy data with precise coordinates (latitude, longitude)
-    data = [
-        {"name": "William Bradford",
-         "lat": 53.521446953950715,
-         "lon": -2.363069184807771,
-         "year": 1625,
-         "color": "red"},
-       
-        {"name": "Wilhelm Constanz",
-         "lat": 50.742462619040104, 
-         "lon": 9.197418362001574,
-         "year": 1900,
-         "color": "blue"},
-        
-        {"name": "Samuel Parsons",
-         "lat": 40.89766494234153, 
-         "lon": -72.73183136944137,
-         "year": 1675,
-         "color": "red"}
-    ]
-
+    
     # Add markers to the map using coordinates
     for person in data:
         folium.CircleMarker(
